b411a1bb116f974107df995bb8d87a4f2ebb2291
Luis M Carballo G <luis.manuel.carballo.gomez@gmail.com>
2024-05-12 15:39:42 -0600

    Implement translations

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import router from './router';
+import i18n from './i18n';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).use(store).mount('#app')
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.use(i18n);
+app.mount('#app');
+
+store.dispatch('socket/initializeWebSocket');
